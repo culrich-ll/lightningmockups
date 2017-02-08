8515fec5c049c019995e3d6cccf8285c42b6db3f
Cecilia Ulrich <cecilia.ulrich@logicline.de>
2017-02-08 12:49:36 +0100

    Uniserv mockups

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
+var port = process.env.PORT || 3000;
 
 router.use(function (req,res,next) {
-  console.log("/" + req.method);
   next();
 });
 
@@ -28,6 +28,6 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(port,function(){
+  console.log("Live at Port "+port);
 });
